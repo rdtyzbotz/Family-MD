99c8732eb80859a4a7041e46f1876a127aaf95f7
Kri.com <107316046+KrizynOfc@users.noreply.github.com>
2022-09-23 14:35:14 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,18 @@ const qrku = "https://telegra.ph/file/f4ee3c55fd67cbd3ea861.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [62895327934887]
-│ • Dana  [62895327934887]
+📍 *BNK MAYBANK*
+No: 008074679622
+
+📍 *Touch nGO* 
+No: 1151905021
+
+📍 *SHOPE PAY*
+ No: +601151905021
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895327934887
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ > ingin run bot? Wa.me/62895327934887
+│ _Hasil donasi akan digunakan buat yg trbaik
 │ _24jam tanpa kendala_
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
