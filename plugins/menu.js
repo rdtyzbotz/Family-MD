edbd218768f628433382362f01e7cee8bcd4b1a8
rdtyzbotz <113617871+rdtyzbotz@users.noreply.github.com>
2022-10-21 13:31:30 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -217,7 +217,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       footer: wm,
       mentions: await conn.parseMention(judul),
       title: '',
-      buttonText: "Klik Disini",
+      buttonText: "Pencet Bang🗿",
       sections
     }
     return conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(judul), contextInfo: { forwardingScore: 99999, isForwarded: true }})
